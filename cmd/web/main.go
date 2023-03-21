7f76e6bd0e7675d85ef5e30cc803855d7b5cc02a
triet <pvmtriet232@gmail.com>
2023-03-21 16:16:33 +0700

    add cmdline flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.Handle("/", http.HandlerFunc(home))
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileserver)))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
