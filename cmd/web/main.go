dc57cb4e3aff27374c206ec8cfe5acf362d6d3d6
triet <pvmtriet232@gmail.com>
2023-03-21 17:48:35 +0700

    add prefix to log

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,10 +4,18 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	Addr      string
+	StaticDir string
+}
+
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	cfg := new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static/", "Path to static assets")
 	flag.Parse()
 
 	mux := http.NewServeMux()
@@ -15,10 +23,13 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileserver := http.FileServer(http.Dir("./ui/static/"))
+	fileserver := http.FileServer(http.Dir(cfg.StaticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileserver)))
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	infolog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorlog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	infolog.Printf("Starting server on %s", cfg.Addr)
+	err := http.ListenAndServe(cfg.Addr, mux)
+	errorlog.Fatal(err)
 }
