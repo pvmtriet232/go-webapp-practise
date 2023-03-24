456c4b2a6f612db763b80052172504bb8adcd70c
triet <pvmtriet232@gmail.com>
2023-03-24 20:49:33 +0700

    add main describe

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,6 +17,9 @@ type application struct {
 }
 
 func main() {
+	// Parsing the runtime configuration settings for the application;
+	// Establishing the dependencies for the handlers; and
+	// Running the HTTP server.
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -27,17 +30,12 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	// Initialize a new http.Server struct. We set the Addr and Handler fields so
-	// that the server uses the same network address and routes as before, and set
-	// the ErrorLog field so that the server now uses the custom errorLog logger in
-	// the event of any problems.
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  app.routes(),
 	}
 	infoLog.Printf("Starting server on %s", *addr)
-	// Call the ListenAndServe() method on our new http.Server struct.
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
