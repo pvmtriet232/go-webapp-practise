92b2591606bafd62069ba479bee42734701da255
triet <pvmtriet232@gmail.com>
2023-03-31 22:33:16 +0700

    update

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -60,6 +60,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+	data := &templateData{Snippet: s}
 	// Initialize a slice containing the paths to the show.page.tmpl file,
 	// plus the base layout and footer partial that we made earlier.
 	files := []string{
@@ -75,7 +76,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 	// And then execute them. Notice how we are passing in the snippet
 	// data (a models.Snippet struct) as the final parameter.
-	err = ts.Execute(w, s)
+	err = ts.Execute(w, data)
 	if err != nil {
 		app.serverError(w, err)
 	}
